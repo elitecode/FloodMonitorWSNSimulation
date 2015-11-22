d5f19793f4fbcad8c7c87a0227d09238f0b73d90
jeevanjot <jeevanjots@iitrpr.ac.in>
2015-11-23 02:51:20 +0530

    Adding Memory

---
+++ src/com/floodmonitoring/MemoryQueue.java
@@ -0,0 +1,27 @@
+package com.floodmonitoring;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Created by sony on 11/23/2015.
+ */
+public class MemoryQueue {
+    Queue<Packet> memQueue;
+    private int size;
+    MemoryQueue(int mSize){
+        memQueue = new ArrayBlockingQueue<Packet>(mSize);
+    }
+    public void addPackets(Packet packet){
+        if(memQueue.size()<5){
+            memQueue.add(packet);
+        }
+        else{
+            memQueue.remove();
+            memQueue.add(packet);
+        }
+    }
+    public boolean inMemory(Packet packet){
+        return memQueue.contains(packet);
+    }
+}
