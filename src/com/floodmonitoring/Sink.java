1849f72d323463b4b4985f66c5ef273a43f00145
savyasachi.sa <savyasachi.sa@gmail.com>
2015-11-27 01:04:00 +0530

    Implementation of Receive & Send of Sink.

--- src/com/floodmonitoring/Sink.java
+++ src/com/floodmonitoring/Sink.java
@@ -8,15 +8,49 @@ public class Sink extends Node {
 	
 	@Override
 	public void receivePacket(Packet packet, long time){
+		if(!memory.inMemory(packet)) {
+			packetQueue.add(packet);
+			log("Received packet", time);
+		}
+		else {
+			log("Received packet already in memory", time);
+		}
+		batteryLevel = Utility.decreaseBattery("RECEIVE", batteryLevel);
+
+		PacketType type = packet.getType();
 		
-	}
-	@Override
-	public boolean Execute(long time){
-		return true;
+		switch(type){
+			case RREQ:
+				Packet responsePacket = new Packet(time, this, PacketType.RREP);
+				responsePacket.addToPath(this);
+				packetQueue.add(responsePacket);
+				break;
+			case STANDARD:
+				//TODO: LOG STUFF and DELAYS
+				break;
+			default: //log packet
+		}
+		
 	}
 	@Override
 	public void sendPacket(Packet packet, long time){
+		packet.updateTravelTime(time);
+		boolean success = true;
+		getCarrierLock();
+
 		
+		switch(packet.getType()) {
+			case RREP:
+				success = sendBroadcast(packet, time);
+				break;
+			default:
+				// log packet
+		}
+		if(success) 
+			batteryLevel = Utility.decreaseBattery("SEND", batteryLevel);
+		else 
+			resetCarrierLock();
+				
 	}
 
 }
