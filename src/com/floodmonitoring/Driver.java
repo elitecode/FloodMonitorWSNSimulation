5f897012329a6b34c6dce6315ec03b5461ed3997
jeevanjot <jeevanjots@iitrpr.ac.in>
2015-11-28 09:04:14 +0530

    driver for diffrent values

--- src/com/floodmonitoring/Driver.java
+++ src/com/floodmonitoring/Driver.java
@@ -1,11 +1,35 @@
 package com.floodmonitoring;
 
+import sun.rmi.runtime.Log;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		Simulator simulator = new Simulator(16);
 		simulator.simulate(100);
+		double[] floodProb = {.2,.4,.6,.8};
+		int[] dataGather = {10,20,30,40};
+		int[] floodtime = {100,200,300,400};
+		double[] Nodefault = {0.0008,0.0016,0.0024,0.0032};
+		for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				for(int k=0;k<4;k++){
+					for(int l=0;l<4;l++){
+						Constants.setFloodProbability(floodProb[i]);
+						Constants.setDataGatherRate(dataGather[j]);
+						Constants.setFloodTimeRate(floodtime[k]);
+						Constants.setNODEFAULTERSEED(Nodefault[l]);
+						System.out.println("Flood Probability = "+floodProb[i]);
+						System.out.println("Data Gather rate = "+dataGather[j]);
+						System.out.println("Flood Time rate = "+floodtime[k]);
+						System.out.println("Node fault Probability = "+Nodefault[l]);
+						simulator.simulate(100);
+					}
+				}
+			}
+		}
 
 	}
+
 
 }
